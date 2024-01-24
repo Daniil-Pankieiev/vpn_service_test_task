d1edd89502a753ad48e8e0f9a69c3a5a66bda20d
Daniil Pankieiev <daniil.pankieiev@gmail.com>
2024-01-24 18:12:37 +0200

    Implemented user form and views for registation and profile using templates

---
+++ accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from accounts.views import (
+    UserDetailView,
+    UserUpdateView, LogoutConfirmation, UserRegisterView,
+)
+
+
+urlpatterns = [
+    path("profile/", UserDetailView.as_view(), name="profile"),
+    path("profile/update/", UserUpdateView.as_view(), name="update_profile"),
+path("accounts/register/", UserRegisterView.as_view(), name="register"),
+    path(
+        "accounts/confirm-logout/",
+        LogoutConfirmation.as_view(),
+        name="confirm_logout",
+    ),
+]
+
+app_name = "account"
